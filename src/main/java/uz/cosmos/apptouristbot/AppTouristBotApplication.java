088487234766270730dba4c66eb7e785cb2eadab
nizom702 <nizom702@gmail.com>
2023-09-28 17:22:55 +0500

    created

---
+++ src/main/java/uz/cosmos/apptouristbot/AppTouristBotApplication.java
@@ -0,0 +1,13 @@
+package uz.cosmos.apptouristbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTouristBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppTouristBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/cosmos/apptouristbot/AppTouristBotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.cosmos.apptouristbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTouristBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
